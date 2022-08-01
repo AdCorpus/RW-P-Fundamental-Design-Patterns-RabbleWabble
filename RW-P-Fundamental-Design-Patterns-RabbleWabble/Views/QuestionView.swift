21072bb12103371853368a2aeac6dceae4ed2e6c
AdPiscinam <lab@walimaloui.com>
2022-08-01 15:14:38 +0200

    Add Question View and connect to StoryBoard

--- RW-P-Fundamental-Design-Patterns-RabbleWabble/Views/QuestionView.swift
+++ RW-P-Fundamental-Design-Patterns-RabbleWabble/Views/QuestionView.swift
@@ -29,5 +29,9 @@
 import UIKit
 
 public class QuestionView: UIView {
-  
+  @IBOutlet var answerLabel: UILabel!
+  @IBOutlet var correctCountLabel: UILabel!
+  @IBOutlet var incorrectCountLabel: UILabel!
+  @IBOutlet var promptLabel: UILabel!
+  @IBOutlet var hintLabel: UILabel!
 }
